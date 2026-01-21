ce7fc0d91f1915b2d4eb83e12a7beb45881fa1fb
naqia88 <mm9130418@gmail.com>
2026-01-21 20:44:36 +0500

    Add C++ example for variable declaration

---
+++ 01-variables/C++_example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int counter = 45;                 // Creates an integer variable
+    double distance = 250.75;         // Creates a floating point variable
+    string student_name = "Firdous";  // Creates a string variable
+
+    cout << "Counter: " << counter << endl;
+    cout << "Distance: " << distance << endl;
+    cout << "Student: " << student_name << endl;
+
+    return 0;
+}
