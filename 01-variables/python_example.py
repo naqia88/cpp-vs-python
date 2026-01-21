761cfac56e447eba3ed32f2e3ff28884577cb0f1
naqia88 <mm9130418@gmail.com>
2026-01-21 20:40:14 +0500

    Add example of variable declaration in Python

---
+++ 01-variables/python_example.py
@@ -0,0 +1,8 @@
+# Variables are created without explicit type declaration
+counter = 45             # Creates an integer variable
+distance = 250.75        # Creates a floating point variable
+student_name = "Firdous" # Creates a string variable
+#Python output:
+print("Counter:", counter)
+print("Distance:", distance)
+print("Student:", student_name)
