1cbad6dce20e6e5689ea97008598078902a9ddf0
Naqia Firdous <mm9130418@gmail.com>
2026-01-27 21:29:06 +0500

    function definitions and calls

--- 05-Functions/C++_example.cpp
+++ 05-Functions/C++_example.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 using namespace std;
 
-//Function overloading
-int multiply(int a, int b){
-    return a * b;
-}
-
-double multiply(double a, double b){
-    return a * b;
-}
-
-//Function with default argument
-int add(int a, int b = 10){
-    return a + b;
-}
+//Function declaration
+int add(int a, int b);
 
 int main(){
-    cout << "Multiply int: " << multiply(5, 6) << endl;       //Output: 30
-    cout << "Multiply double: " << multiply(2.5, 4.0) << endl; //Output: 10
-
-    cout << "Add with 2 args: " << add(5, 15) << endl;        //Output: 20
-    cout << "Add with 1 arg: " << add(5) << endl;             //Output: 15
+    int x = 10, y = 20;
+    
+    //Function call
+    int sum = add(x, y);
+    cout << "Sum: " << sum << endl;  //Output: 30
 
     return 0;
+}
+
+//Function definition
+int add(int a, int b){
+    return a + b;
 }
