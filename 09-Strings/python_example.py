f2da02c732e42041e98d2a2aa4b172854d2ee901
Naqia Firdous <mm9130418@gmail.com>
2026-02-02 22:39:27 +0500

    Add python_example.py for string operations

---
+++ 09-Strings/python_example.py
@@ -0,0 +1,17 @@
+text = "Data Science"
+
+#Length
+print("Length:", len(text))  #Output: 12
+
+#Concatenation
+result = text + " Rocks"
+print(result)  #Output: Data Science Rocks
+
+#Slicing
+print(text[0:4])  #Output: Data
+
+#Search
+print(text.find("Science"))  #Output: 5
+
+#Uppercase
+print(text.upper())  #Output: DATA SCIENCE
