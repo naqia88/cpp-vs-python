abf8af4868ee1c74c9f53216195fb7e884804a57
Naqia Firdous <mm9130418@gmail.com>
2026-02-26 21:42:08 +0500

    Create python_example.py

---
+++ 25-Insertion_&_Selection_Sort/python_example.py
@@ -0,0 +1,26 @@
+#Selection Sort
+def selection_sort(arr):
+    for i in range(len(arr) - 1):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+#Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+
+selection_sort(arr)   
+insertion_sort(arr)   
+
+print("Sorted Array:", arr)
